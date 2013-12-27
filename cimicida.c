67626d8bebbdf45aa52196f8813dc4f952ab84e0
Eduardo Tongson <propolice@gmail.com>
2013-12-27 08:17:41 +0800

    Follow style(9) convention.

--- cimicida.c
+++ cimicida.c
@@ -1,11 +1,14 @@
+
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
+
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
 
-static int lchroot (lua_State *L)
+static int
+cchroot(lua_State *L)
 {
     const char *path = luaL_checkstring(L, 1);
     if (chroot(path) == -1) {
@@ -17,7 +20,8 @@ static int lchroot (lua_State *L)
     return 1;
 }
 
-static int lchdir (lua_State *L)
+static int
+cchdir(lua_State *L)
 {
     const char *path = luaL_checkstring(L, 1);
     if (chdir(path) == -1) {
@@ -29,14 +33,18 @@ static int lchdir (lua_State *L)
     return 1;
 }
 
-static const luaL_Reg syslib[] = {
-  {"chroot", lchroot},
-  {"chdir", lchdir},
-  {NULL, NULL}
+static const
+luaL_Reg syslib[] =
+{
+    {"chroot", cchroot},
+    {"chdir", cchdir},
+    {NULL, NULL}
 };
 
-LUALIB_API int luaopen_cimicida(lua_State *L) {
-  luaL_newlib(L, syslib);
-  return 1;
+LUALIB_API int
+luaopen_cimicida(lua_State *L)
+{
+    luaL_newlib(L, syslib);
+    return 1;
 }
 
